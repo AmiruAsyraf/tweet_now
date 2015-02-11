c9867abdf9557b9c12225290bcc399b897bc6bc1
AmiruAsyraf <amiruasyraf@gmail.com>
2015-02-11 11:16:20 +0800

    AJAX done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,21 @@ get '/' do
   erb :index
 end
 
+# get '/tweet/:result' do
+# @respond = params[:result]
+
+#  erb :result
+# end
+
 post '/tweet' do
-  $client.update(params[:tweet]) ? @respond = "Success" : @respond = "Failed"
+  result = $client.update(params[:tweet])
+
+  if result
+    @respond = "You have successfully tweeted"
+  else
+    @respond = "Empty tweet or more than 140 char"
+  end
+
+
   erb :result
 end
--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,24 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
+
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('.search').click(function(event){
+    var post = $('.tweets').val();
+    $('#loading').show();
+
+    $.ajax({
+      type:"POST",
+      url: "/tweet",
+      data: { "tweet" : post},
+      success: function(data){
+       $('#loading').hide();
+       $('#result').html("");
+       $('#result').append('<p>' + data + '</p>');
+    },
+});
+event.preventDefault();
+
+  })
 });
